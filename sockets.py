2e479338d444d3e08860b3e5d941cf449530617f
Sam Val <samvalentine2811@gmail.com>
2021-03-27 23:55:57 +0700

    udapte

--- sockets.py
+++ sockets.py
@@ -1,7 +1,19 @@
 from chat import socketio
 import sys
+import json
+from flask_login import current_user
 
 @socketio.on('message')
 def message(data):
     print(f'{data}', file=sys.stderr)
     socketio.send(data)
+
+@socketio.on('message_created')
+def message_created(data):
+    print(f'user just made: {data}', file=sys.stderr)
+    processed_data = f'<b>{current_user.username}</b>: {data["text"]}'
+    socketio.emit('message_created', processed_data)
+
+@socketio.on('load_history')
+def load_history(room_id):
+    pass
--- static/socket.js
+++ static/socket.js
@@ -1,12 +1,30 @@
 document.addEventListener('DOMContentLoaded', () => {
+    const btn_enter = document.querySelector('#btn-send')
+    const chat_display = document.querySelector('#chat_history_display')
+    var text_area = document.querySelector('#text-message')
+
     var socket = io.connect('http://' + document.domain + ':' + location.port);
 
       socket.on('connect', () => {
+          var room_id = 0;
           console.log('connected');
-          socket.send("You're connected");
+          socket.emit('load_history',room_id)
       })
 
       socket.on('message', data => {
           console.log(`message received: ${data}`)
       })
+
+      socket.on('message_created', data => {
+            var new_message_div = document.createElement('div');
+            new_message_div.innerHTML = data;
+            chat_display.appendChild(new_message_div);
+      })
+
+    btn_enter.addEventListener('click', ()=> {
+        let current_text = text_area.value.trim();
+
+        socket.emit('message_created', {text: current_text});
+    })
+
 })
